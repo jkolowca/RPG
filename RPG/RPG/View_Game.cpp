b82db62baf87338802feac887bd69b8538523f3a
Marlena Brzeziok <marel1@autograf.pl>
2019-06-13 18:59:22 +0200

    improving the character

--- RPG/RPG/View_Game.cpp
+++ RPG/RPG/View_Game.cpp
@@ -2,7 +2,7 @@
 #include "ViewManager.h"
 #include <iostream>
 
-View_Game::View_Game(ViewManager* _manager) : View(_manager), map(manager->GetShared()), player(manager->GetShared(),1) {
+View_Game::View_Game(ViewManager* _manager) : View(_manager), map(manager->GetShared()), player(manager->GetShared(),0) {
 	map.Load(2);
 	player.SetPosition(map.getPlayerPosition());
 }
