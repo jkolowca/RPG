c32688165b061fc4c9076893c8f11951bc45f52f
jkolowca <jkolowca@users.noreply.github.com>
2019-06-16 13:52:42 +0200

    map finally shifting its position ;)

--- RPG/RPG/View_Game.cpp
+++ RPG/RPG/View_Game.cpp
@@ -32,6 +32,7 @@ void View_Game::Deactivate() {
 
 void View_Game::Update() {
 	//player.Update();
+	map.Update();
 	entityMgr.Update();
 }
 
@@ -44,7 +45,7 @@ void View_Game::Draw() {
 }
 
 void View_Game::Position() {
-	map.MakeMove(0, 0);
+	map.Move({ 0, 0 });
 	entityMgr.Find(0)->setPosition( map.getPlayerPosition());
 }
 
@@ -58,7 +59,7 @@ void View_Game::Escape(sf::Event::KeyEvent) {
 void View_Game::Up(sf::Event::KeyEvent) {
 	//if(!player.isMoving())
 	if (!entityMgr.Find(0)->isMoving())
-		if (map.MakeMove(0, -1))
+		if (map.MakeMove( 0, -1 ))
 			//player.move(map.getPlayerShift(), direction::Up);
 			entityMgr.Find(0)->Move(map.getPlayerShift(), animation_type::Up);
 }
